ce289dd0ac911f76799da32c0f42ac094e9178d9
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-05-12 16:02:31 +0900

    Go言語プログラムの開発ができることを確認するためのテストコードを作成。

--- channel/main.go
+++ channel/main.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	var (
-		ch1 chan byte
+		ch1 chan byte = make(chan byte, 1)
 	)
 
+	fmt.Println(len(ch1))
 	ch1 <- 0x00
+	fmt.Println(len(ch1))
+	time.Sleep(3 * time.Second)
+	res := <-ch1
+	fmt.Println("result:", res)
 }
